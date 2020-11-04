6ee1ee68beb9cc0fe5672d256897beb29b2ab984
Dmitriy Derepko <xepozz@list.ru>
2020-11-04 20:47:26 +0300

    Add base configuration

---
+++ config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiisoft/user' => [
+        'authUrl' => '/login'
+    ],
+];
---
+++ config/web.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Auth\AuthenticationMethodInterface;
+use Yiisoft\User\UserAuth;
+
+/**
+ * @var array $params
+ */
+
+return [
+    UserAuth::class => [
+        '__class' => UserAuth::class,
+        'withAuthUrl()' => [$params['yiisoft/user']['authUrl']]
+    ],
+
+    AuthenticationMethodInterface::class => UserAuth::class
+];
