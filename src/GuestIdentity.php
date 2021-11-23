d89549c24b798598ee64e7cfea659e9298e923b2
Evgeniy Zyubin <mail@devanych.ru>
2021-11-23 23:32:26 +0300

    Fix #25: Make GuestIdentity extensible (#41)

--- src/GuestIdentity.php
+++ src/GuestIdentity.php
@@ -9,9 +9,9 @@ use Yiisoft\Auth\IdentityInterface;
 /**
  * Implementation of the identity interface for a guest non-authenticated user.
  */
-final class GuestIdentity implements IdentityInterface
+class GuestIdentity implements IdentityInterface
 {
-    public function getId(): ?string
+    final public function getId(): ?string
     {
         return null;
     }
