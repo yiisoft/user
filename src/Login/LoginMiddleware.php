8294cd722e41e84dfdf74ecf950eff180bc4e61d
Evgeniy Zyubin <mail@devanych.ru>
2021-12-20 19:25:23 +0300

    Fix #22: Add login middleware (#47)
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

---
+++ src/Login/LoginMiddleware.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\User\Login;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
+use Yiisoft\Auth\IdentityInterface;
+use Yiisoft\Auth\Middleware\Authentication;
+use Yiisoft\User\CurrentUser;
+
+/**
+ * `LoginMiddleware` automatically logs user in if {@see IdentityInterface} instance presents in a request
+ * attribute. It is usually put there by {@see \Yiisoft\Auth\Middleware\Authentication}.
+ */
+final class LoginMiddleware implements MiddlewareInterface
+{
+    private CurrentUser $currentUser;
+    private LoggerInterface $logger;
+
+    /**
+     * @param CurrentUser $currentUser The current user instance.
+     * @param LoggerInterface $logger The logger instance.
+     */
+    public function __construct(CurrentUser $currentUser, LoggerInterface $logger)
+    {
+        $this->currentUser = $currentUser;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Before this middleware, there should be {@see \Yiisoft\Auth\Middleware\Authentication} in the middleware stack.
+     * It authenticates the user and places {@see IdentityInterface} instance in the corresponding request attribute.
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (!$this->currentUser->isGuest()) {
+            return $handler->handle($request);
+        }
+
+        /** @var mixed $identity */
+        $identity = $request->getAttribute(Authentication::class);
+
+        if ($identity instanceof IdentityInterface) {
+            $this->currentUser->login($identity);
+        } else {
+            $this->logger->warning('Unable to authenticate user by token. Identity not found.');
+        }
+
+        return $handler->handle($request);
+    }
+}
---
+++ tests/Login/LoginMiddlewareTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\User\Tests\Login;
+
+use HttpSoft\Message\ServerRequest;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Auth\Middleware\Authentication;
+use Yiisoft\Test\Support\EventDispatcher\SimpleEventDispatcher;
+use Yiisoft\User\Event\AfterLogin;
+use Yiisoft\User\Event\BeforeLogin;
+use Yiisoft\User\Guest\GuestIdentity;
+use Yiisoft\User\Login\LoginMiddleware;
+use Yiisoft\User\Tests\Support\LastMessageLogger;
+use Yiisoft\User\CurrentUser;
+use Yiisoft\User\Tests\Support\MockIdentity;
+use Yiisoft\User\Tests\Support\MockIdentityRepository;
+
+final class LoginMiddlewareTest extends TestCase
+{
+    private const IDENTITY_ID = 'test-id';
+
+    private MockIdentity $identity;
+    private LastMessageLogger $logger;
+    private SimpleEventDispatcher $eventDispatcher;
+    private CurrentUser $currentUser;
+
+    protected function setUp(): void
+    {
+        $this->identity = new MockIdentity(self::IDENTITY_ID);
+        $this->logger = new LastMessageLogger();
+        $this->eventDispatcher = new SimpleEventDispatcher();
+        $this->currentUser = new CurrentUser(new MockIdentityRepository(), $this->eventDispatcher);
+    }
+
+    public function testCorrectLogin(): void
+    {
+        $middleware = new LoginMiddleware($this->currentUser, $this->logger);
+
+        $middleware->process($this->createServerRequest(), $this->createRequestHandler());
+
+        $this->assertNull($this->logger->getLastMessage());
+        $this->assertInstanceOf(MockIdentity::class, $this->currentUser->getIdentity());
+        $this->assertSame(self::IDENTITY_ID, $this->currentUser->getIdentity()->getId());
+    }
+
+    public function testCorrectProcessWithNonGuestUser(): void
+    {
+        $currentUser = new CurrentUser(new MockIdentityRepository($this->identity), $this->eventDispatcher);
+        $middleware = new LoginMiddleware($currentUser, $this->logger);
+
+        $middleware->process($this->createServerRequest(), $this->createRequestHandler());
+
+        $this->assertNull($this->logger->getLastMessage());
+        $this->assertInstanceOf(MockIdentity::class, $currentUser->getIdentity());
+        $this->assertSame(self::IDENTITY_ID, $currentUser->getIdentity()->getId());
+        $this->assertCount(2, $this->eventDispatcher->getEvents());
+        $this->assertSame([BeforeLogin::class, AfterLogin::class], $this->eventDispatcher->getEventClasses());
+
+        $middleware->process($this->createServerRequest(), $this->createRequestHandler());
+
+        $this->assertNull($this->logger->getLastMessage());
+        $this->assertInstanceOf(MockIdentity::class, $currentUser->getIdentity());
+        $this->assertSame(self::IDENTITY_ID, $currentUser->getIdentity()->getId());
+        $this->assertCount(2, $this->eventDispatcher->getEvents());
+        $this->assertSame([BeforeLogin::class, AfterLogin::class], $this->eventDispatcher->getEventClasses());
+    }
+
+    public function testIdentityNotFound(): void
+    {
+        $middleware = new LoginMiddleware($this->currentUser, $this->logger);
+
+        $middleware->process($this->createServerRequest(false), $this->createRequestHandler());
+
+        $this->assertInstanceOf(GuestIdentity::class, $this->currentUser->getIdentity());
+        $this->assertNull($this->currentUser->getIdentity()->getId());
+        $this->assertSame('Unable to authenticate user by token. Identity not found.', $this->logger->getLastMessage());
+    }
+
+    private function createServerRequest(bool $withIdentity = true): ServerRequestInterface
+    {
+        return (new ServerRequest())->withAttribute(Authentication::class, $withIdentity ? $this->identity : null);
+    }
+
+    private function createRequestHandler(): RequestHandlerInterface
+    {
+        $requestHandler = $this->createMock(RequestHandlerInterface::class);
+
+        $requestHandler
+            ->expects($this->once())
+            ->method('handle')
+        ;
+
+        return $requestHandler;
+    }
+}
